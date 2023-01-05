b50dbaa326696002e6f50b35801a99fc72e06009
md5dalton <md5daltonggs@gmail.com>
2023-01-05 14:55:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,16 +25,19 @@ export default function Home() {
         <section className="card">
           <div className="card-content">
             <Image
-              src="/images/image-victor.jpg"
-              width={96}
-              height={96}
-              alt="headshot of a man in a shirt and jacket with a mountainous blurred background"
+                className="profile-image"
+                src="/images/image-victor.jpg"
+                width={96}
+                height={96}
+                alt="headshot of a man in a shirt and jacket with a mountainous blurred background"
             />
             <div className="details">
-              <h1>Ntate Mpiti</h1>
-              <span>26</span>
+                <div className="name">
+                    <h1>Ntate Mpiti</h1>
+                    <span>26</span>
+                </div>
+                <h3 className="city">Qacha's Nek</h3>
             </div>
-            <h3 className="city">Qacha's Nek</h3>
             <ul className="stats" role="list">
                 <Stat name="followers" count="80k" />
                 <Stat name="likes" count="803k" />
